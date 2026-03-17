a9b1066347102084eaf5a73133c4af3fb3521cef
luancarneiroo <luanncarneiro08@gmail.com>
2026-03-17 11:41:15 -0300

    aula 02

---
+++ Aula02-VariaveisEOperadores/Aula02Desafio.py
@@ -0,0 +1,11 @@
+# DESAFIO!!!
+nome = input("Digite seu nome:")
+print("Boas-Vindas", nome,"!!")
+
+datanasc = input("Diga sua data de nascimento:")
+print(datanasc)
+
+numero1 = input("Número 1:")
+numero2 = input("Número 2:")
+resultado = numero1 + numero2
+print(resultado)
---
+++ Aula02-VariaveisEOperadores/Aula02a.py
@@ -0,0 +1,29 @@
+print("Olá Mundo")
+
+print (7+4)
+print("7+4")
+print ("7"+"4")
+
+# Comentários de uma linha
+'''Comentarios de Múltiplas Linhas
+Tiiiipooo
+assimmmm'''
+
+# VARIAVEIS
+
+nome = "Alexandre"
+idade = 19
+peso = 55.9
+print (nome,idade,peso)
+
+
+#IMPUT -- SIMULA FORMS
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+peso = float(input("Digite seu peso:"))
+
+print(nome, idade, peso)
+
+
+
+
